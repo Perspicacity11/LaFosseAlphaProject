973601fb0dde8b1b62bfbe4ae1875ea59266b83b
Perspicacity11 <cassius.cr.naylor@gmail.com>
2025-04-18 13:10:17 +0100

    Split routes into user and session routers, configure api.js and index.js files to initiate server on run command, add logger middleware

--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -1,14 +1,7 @@
 const {Router} = require ("express")
 const userController = require("../controllers/user_controllers")
-const sessionController = require("../controllers/session_controllers")
 
 const userRouter = Router()
-const sessionRouter = Router ()
-
-sessionRouter.get("/", sessionController.index)
-sessionRouter.get("/:id", sessionController.show)
-sessionRouter.post("/", sessionController.create)
-sessionRouter.delete("/:id", sessionController.destroy)
 
 userRouter.get("/", userController.index)
 userRouter.get("/:id", userController.show)
@@ -16,8 +9,6 @@ userRouter.post("/", userController.create)
 userRouter.patch("/:id", userController.update)
 userRouter.delete("/:id", userController.destroy)
 
-module.exports = {
-    userRouter, sessionRouter
-}
+module.exports = userRouter;
 
 
