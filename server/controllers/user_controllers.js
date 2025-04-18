ced05ae33f09a73e9674655f5e842ec5f67bc7e7
academy <academy@LFA-FVFG2N8EQ05D.local>
2025-04-18 11:37:06 +0100

    Add/Update user_controllers.js

---
+++ server/controllers/user_controllers.js
@@ -0,0 +1,65 @@
+const User = require("../models/user_models")
+
+async function index(req, res) {
+    try {
+        const user = await User.getAll()
+        res.status(200).json(user)
+    } catch(err){
+        res.status(500).json({ error: err.message})
+    }
+}
+
+async function show(req, res) {
+    try {
+        const id = parseInt(req.params.id)
+        const user = await User.getOneById(id)
+        res.status(200).json(user)
+    } catch(err) {
+        res.status(404).json({ error: err.message })
+    }
+}
+
+async function create(req, res) {
+    try {
+        const data = req.body
+        const user = await User.create(data)
+        res.status(201).json(user)
+    } catch(err) {
+        res.status(404).json({ error: err.message})
+    }
+}
+
+async function update(req, res) {
+    try {
+        const id = parseInt(req.params.id)
+        const data = req.body
+        const user = await User.getOneById(id)
+        const result = await user.update(data)
+        res.status(200).json(result)
+    } catch(err) {
+        res.status(404).json({ error: err.message})
+    }
+}
+
+async function destroy(req, res) {
+    try{
+    const id = parseInt(req.params.id)
+    const user = await User.getOneById(id)
+    const result = await User.destroy()
+    res.status(200).json(result)
+    } catch(err) {
+        res.status(404).json({ error: err.message })
+    }
+}
+
+module.exports = {
+    index, show, create, update, destroy
+}
+
+
+
+
+
+
+
+
