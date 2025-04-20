139ee6211da283a0b0ac2a584bef22df459084d7
Ed Pond <huri1965@hotmail.com>
2025-04-20 12:02:20 +0100

    Add background images and country.html

--- client/app.js
+++ client/app.js
@@ -1,9 +1,2 @@
-let startBtn = document.getElementById('startGame')
-let options = document.querySelectorAll('.options')
 
-startBtn.addEventListener('click', () => {
-    options.forEach(dot => {
-        dot.style.display = "flex"
-    })
-    startBtn.style.display = "none"
-})
+
