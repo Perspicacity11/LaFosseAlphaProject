d32549b243fd28b2661a4ba982b1fd1c668747c9
Ed Pond <huri1965@hotmail.com>
2025-04-20 14:08:52 +0100

    change background image for country.html and thicken the border for map svg

--- client/app.js
+++ client/app.js
@@ -17,13 +17,13 @@ function findCountry(e){
             const bbox = path.getBBox();
             const x = bbox.x + bbox.width / 2;
             const y = bbox.y + bbox.height / 2;
-            path.style.fill = "pink"
-            const text = document.createElementNS("http://www.w3.org/2000/svg","text");
-            text.setAttribute("x", x);
-            text.setAttribute("y", y);
-            text.setAttribute("font-size", "10");
-            text.setAttribute("fill", "red"); // or white depending on your map
-            text.setAttribute("pointer-events", "none"); // so it doesn't interfere with clicks
+            path.style.fill = "#fad2e1"
+            const text = document.createElementNS("http://www.w3.org/2000/svg","text")
+            text.setAttribute("x", x)
+            text.setAttribute("y", y)
+            text.setAttribute("font-size", "10")
+            text.setAttribute("fill", "red")
+            text.setAttribute("pointer-events", "none")
             text.textContent = e.target.userInput.value;
             svg.appendChild(text);
         }
