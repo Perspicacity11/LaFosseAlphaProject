d2720cf12961836c6437320a6ae32d1f3cb5041e
Ed Pond <huri1965@hotmail.com>
2025-04-20 13:44:48 +0100

    create function find Country

--- client/app.js
+++ client/app.js
@@ -1,2 +1,35 @@
+const countryInputForm = document.getElementById('countryForm')
+const allPath = document.querySelectorAll(".allPath")
+const svg = document.querySelector(".svg");
+
+countryInputForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    findCountry(e)
+    e.target.userInput.value = ""
+    
+})
+
+
+function findCountry(e){
+    allPath.forEach(path => {
+        const countryName = path.getAttribute("name")
+        if(e.target.userInput.value.toLowerCase() === countryName.toLowerCase()){
+            const bbox = path.getBBox();
+            const x = bbox.x + bbox.width / 2;
+            const y = bbox.y + bbox.height / 2;
+            path.style.fill = "pink"
+            const text = document.createElementNS("http://www.w3.org/2000/svg","text");
+            text.setAttribute("x", x);
+            text.setAttribute("y", y);
+            text.setAttribute("font-size", "10");
+            text.setAttribute("fill", "red"); // or white depending on your map
+            text.setAttribute("pointer-events", "none"); // so it doesn't interfere with clicks
+            text.textContent = e.target.userInput.value;
+            svg.appendChild(text);
+        }
+    })
+}
+
+
 
 
