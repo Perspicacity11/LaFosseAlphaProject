7b5b4999cebdd8899bac0d4d661b8be799fcd975
academy <academy@LFA-FVFG2N8EQ05D.local>
2025-04-23 10:21:55 +0100

    Merge Production & Update user controller/route/authmiddleware

--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -1,14 +1,10 @@
-const {Router} = require ("express")
-const userController = require("../controllers/user_controllers")
+const { Router } = require('express');
 
-const userRouter = Router()
+const userController = require('../controllers/user_controllers');
 
-userRouter.get("/", userController.index)
-userRouter.get("/:id", userController.show)
-userRouter.post("/", userController.create)
-userRouter.patch("/:id", userController.update)
-userRouter.delete("/:id", userController.destroy)
+const userRouter = Router();
+
+userRouter.post("/register", userController.signup);
+userRouter.post("/login", userController.login);
 
 module.exports = userRouter;
-
-
