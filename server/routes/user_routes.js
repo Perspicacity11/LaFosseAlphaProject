bf6e7e1977e79dcbeb1bb96412716e568f9c7dc7
Perspicacity11 <cassius.cr.naylor@gmail.com>
2025-04-23 17:00:44 +0100

    Merge Kyaw dev branch with register/login functions to production branch

--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -1,10 +1,15 @@
-const { Router } = require('express');
+const {Router} = require ("express")
+const userController = require("../controllers/user_controllers")
 
-const userController = require('../controllers/user_controllers');
+const userRouter = Router()
 
-const userRouter = Router();
-
+userRouter.get("/", userController.index)
+userRouter.get("/:id", userController.show)
+userRouter.patch("/:id", userController.update)
+userRouter.delete("/:id", userController.destroy)
 userRouter.post("/register", userController.signup);
 userRouter.post("/login", userController.login);
+
+//add Kyaw user routes can in
 
 module.exports = userRouter;
