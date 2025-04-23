bb649cf407634c8e06026d7223d3b928fa8c9b51
Ed Pond <huri1965@hotmail.com>
2025-04-23 12:29:09 +0100

    define login.js to fetch user api to login page

---
+++ client/login.js
@@ -0,0 +1,37 @@
+  const inputUsername = document.getElementById("inputUsername")
+  const inputPassword = document.getElementById("inputPassword")
+  const loginBtn = document.getElementById("loginButton")
+  const welcomeMsg = document.querySelector(".welcome-message")
+
+  loginBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+
+    try{
+        const response = await fetch("/users/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              username : inputUsername.value,
+              password : inputPassword.value
+            })
+          })
+
+          const data = await response.json()
+
+          if (response.ok) {
+              console.log("Login successful:", data);
+              window.location.href = "./index.html";
+              welcomeMsg.innerHTML = `Welcome ${username}`
+            } else {
+              console.error("Login failed:", data.error);
+              alert("Invalid login credentials!");
+            }
+        }catch(err){
+            console.log("Can't retrieve data")
+        }
+        
+  })
+  
+ 
+
+
