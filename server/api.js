ec2ba82e00e2f2daed54e2152b4b41ba2e2704fd
Perspicacity11 <cassius.cr.naylor@gmail.com>
2025-05-05 12:55:22 +0100

    Adjust CORS configuration

--- server/api.js
+++ server/api.js
@@ -12,17 +12,21 @@ const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}))
+
+app.use(cors(
+//   {
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true
+// }
+))
+
 app.use(logger)
 app.use('/users', userRouter);
 app.use('/sessions', sessionRouter);
