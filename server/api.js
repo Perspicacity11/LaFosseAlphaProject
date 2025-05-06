6c50af01d6be037833bc1c99f84f1e6c304acb83
Perspicacity11 <cassius.cr.naylor@gmail.com>
2025-05-06 10:05:50 +0100

    Reordered api.js

--- server/api.js
+++ server/api.js
@@ -8,14 +8,14 @@ const sessionRouter = require('./routes/session_routes');
 
 const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(cors({
-  origin: ['http://localhost:3000/', 'https://geo-nius.netlify.app/'],
+  origin: ['https://geo-nius.netlify.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(logger)
 app.use('/users', userRouter);
